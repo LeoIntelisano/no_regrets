1bdb1e58221b6b3fa8d8af266c9b1ebc380141a9
Leo Intelisano <leointel@umich.edu>
2024-06-19 12:51:37 +0200

    mbr: fix double print issue. Add hang

--- LEOS/src/arch/i386/mbr/bootsec.asm
+++ LEOS/src/arch/i386/mbr/bootsec.asm
@@ -4,21 +4,24 @@
 
 start:
 	xor ax, ax
-;	mov cs, ax
 	mov ds, ax
 	mov es, ax
-;	mov ss, ax
 
 	mov bp, 0x9000 ; set the stack to usable memory 0x7E00 to 0x7FFFF
 	mov sp, bp
 	mov bx, KERNEL_LOADING
 	call print_rm
-
+	
+	mov bx, PRINT_CHECK
+	call print_rm
+hang:
+	jmp hang
 ; includes
 %include "print_rm.asm"
 
 ; globals
-KERNEL_LOADING db "loading kernel...", 0
+KERNEL_LOADING db "loading kernel...",0
+PRINT_CHECK db "check 1, 2",0
 
 times(510 - ($-$$)) db 0
 
--- LEOS/src/arch/i386/mbr/print_rm.asm
+++ LEOS/src/arch/i386/mbr/print_rm.asm
@@ -1,19 +1,17 @@
 ; function that prints a string from the address in bx at the segment in ds
 print_rm:
-				pusha
-				mov ah, 0x0e
-				xor si, si	
+	pusha
+	mov ah, 0x0e
+	xor si, si	
 
 print_loop:
-				mov al, [bx+si]
-				cmp al, 0
-				je done
-				int 0x10
-				inc si
-				jmp print_loop
+	mov al, [bx+si]
+	cmp al, 0
+	je done
+	int 0x10
+	inc si
+	jmp print_loop
 done:
-				popa
-				ret
-
-
+	popa
+	ret
 
