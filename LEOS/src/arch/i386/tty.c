0192a77ea93ea814331f9f0f0d4cc583b9cfc930
Leo Intelisano <leointel@umich.edu>
2024-06-28 12:57:02 +0200

    TTY: Add scrolling. Change putc implementation to use putc_at

--- LEOS/src/arch/i386/tty.c
+++ LEOS/src/arch/i386/tty.c
@@ -20,16 +20,42 @@ size_t tty_col = 0;
 vga_t vga = {0xb, 0x0, 0x0};
 volatile uint8_t* vga_mem = (volatile uint8_t*)0xb8000;
 
+static void tty_clear_r(size_t r){
+	for (size_t i = 0; i < VGA_WIDTH; i++) {
+		vga_mem[index(i, r)] = ' ';
+		vga_mem[index(i, r)+1] = *(uint8_t*)&vga;
+	}
+}
+
+static void tty_putc_at(int ch, size_t x, size_t y){
+	*(volatile uint16_t*)&vga_mem[index(x, y)] =  (uint8_t)ch | (*(uint8_t*)&vga) << 8;
+}
+// TODO: fix to copy the attribute bytes as well
+static void tty_scroll(){
+	for (size_t r = 1; r < VGA_HEIGHT; r++){
+		for (size_t c = 0; c < VGA_WIDTH; c++) {
+			tty_putc_at(vga_mem[index(c, r)], c, r-1);
+		}
+	}
+	tty_clear_r(VGA_HEIGHT-1);
+	tty_row = VGA_HEIGHT-1;
+	tty_col = 0;
+}
 int tty_putc(int ch) {	
 	if (tty_col >= VGA_WIDTH) {
 		tty_col = 0;
 		tty_row++;
 	}
 	if (tty_row >= VGA_HEIGHT) {
-		tty_row = 0;	
+		tty_scroll();
 	}
-
-	*(volatile uint16_t*)&vga_mem[index(tty_col++, tty_row)] =  (uint8_t)ch | (*(uint8_t*)&vga) << 8;
+	// format checks
+	if (ch == '\n'){
+		tty_row++;
+		tty_col = 0;
+		return '\n';
+	}
+	tty_putc_at(ch, tty_col++, tty_row);
 	return ch;
 }
 
@@ -53,9 +79,8 @@ static void tty_top() {
 
 void tty_clear() {
 	tty_top();
-	for (size_t i = 0; i < 2*VGA_WIDTH*VGA_HEIGHT; i++){
-		vga_mem[i++] = ' ';
-		vga_mem[i] = *(uint8_t*)&vga;
+	for (size_t i = 0; i < VGA_HEIGHT; i++){
+		tty_clear_r(i);
 	}
 }
 
