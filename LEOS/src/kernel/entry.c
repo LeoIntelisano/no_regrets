2f8a0a42ac56652ab67513a05763ddaf965034f8
Leo Intelisano <leointelisano@Leos-MacBook-Air-2.local>
2025-02-26 14:16:53 -0500

    MERGE: Finish merge

--- LEOS/src/kernel/entry.c
+++ LEOS/src/kernel/entry.c
@@ -3,7 +3,6 @@
 #include "io/io.h"
 
 void _cstart() {
-<<<<<<< HEAD
 	tty_print("Hello, Kernel World!\n");
 	uint8_t test = _test_ams();
 	if (test == 1) {
@@ -12,8 +11,4 @@ void _cstart() {
 	if (test == 4) {
 		tty_print("The asm stuff worked!?\n");
 	}
-=======
-	tty_clear();	
-	tty_print("Welcome to LEOS");
->>>>>>> 8d6131bb19d0c2f12c8412e8a780f7565c0e381f
 }
