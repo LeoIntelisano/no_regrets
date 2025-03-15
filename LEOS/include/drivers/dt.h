4afdcabed99b3f1c057c50b2116bb90b7dd7d5ab
Leo Intelisano <leointel@umich.edu>
2025-03-15 00:31:50 -0400

    LINK: Delete link test sources

--- LEOS/include/drivers/dt.h
+++
@@ -1,7 +0,0 @@
-#ifndef DT_H
-#define DT_H
-
-
-void link_test();
-
-#endif //DT_H
--- LEOS/src/drivers/dt.c
+++
@@ -1,6 +0,0 @@
-#include <drivers/dt.h>
-#include <kernel/tty.h>
-
-void link_test() {
-    tty_print("Drivers File Got Linked Properly\n");
-}
