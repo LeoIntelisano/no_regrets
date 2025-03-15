3efe27e72e6f9946c823db0187e8121c7ca4d140
Leo Intelisano <leointel@umich.edu>
2025-03-15 00:31:02 -0400

    MEM_MAP: Fix incorrect memory map structure

--- LEOS/include/kernel/memory_map.h
+++ LEOS/include/kernel/memory_map.h
@@ -1,17 +1,21 @@
-#ifndef MEMORY_H
-#define MEMORY_H
+#ifndef MEMORY_MAP_H
+#define MEMORY_MAP_H
 #include <stdint.h>
 
+typedef enum bios_availability_enum {
+    __xxx__, USABLE, RESERVED  
+}bios_availability;
+
 typedef struct bios_mem_map_str{
-    uint16_t start_addr_h;
-    uint16_t start_addr_l;
-    uint16_t end_addr_h;
-    uint16_t end_addr_l;
-    uint16_t mem_size;
+    uint32_t start_addr_l;
+    uint32_t start_addr_h;
+    uint32_t len_l;
+    uint32_t len_h;
+    uint32_t mem_type;
 }bios_mem_map;
 
 // returns the total number of entries written, or 0 if it failed or didn't write any bytes
 // params: pointer to mem_map array, size in bytes of array
 int get_memory_map(bios_mem_map*, int size);
 
-#endif // MEMORY_H
+#endif // MEMORY_MAP_H
--- LEOS/src/arch/i386/memory_map.c
+++ LEOS/src/arch/i386/memory_map.c
@@ -1,5 +1,5 @@
 #include <stddef.h>
-#include "kernel/memory_map.h"
+#include <kernel/memory_map.h>
 
 // TODO find some way to abstract this interface
 
@@ -8,15 +8,15 @@
 // TODO swap to kernel memcpy
 int get_memory_map(bios_mem_map* mm, int size) {
     int temp = 0;
-    uint16_t* addr = (uint16_t*)MEM_MAP_ADDR;
+    uint32_t* addr = (uint32_t*)MEM_MAP_ADDR;
     while (*addr != 0xdead) {
         if (temp + (int)sizeof(bios_mem_map) > size)
             return 0;
-        mm->start_addr_h = *addr++;
         mm->start_addr_l = *addr++;
-        mm->end_addr_h = *addr++;
-        mm->end_addr_l = *addr++;
-        mm->mem_size = *addr++;
+        mm->start_addr_h = *addr++;
+        mm->len_l = *addr++;
+        mm->len_h = *addr++;
+        mm->mem_type = *addr++;
         mm++;
         temp+= sizeof(bios_mem_map);
     }
